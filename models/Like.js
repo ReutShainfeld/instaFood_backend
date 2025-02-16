23ed84fe2f349ced4453fb12e5256a6d4d623f62
ChavaL1s <chavaishere@gmail.com>
2025-02-16 22:12:32 +0200

    my fist commit

--- models/Like.js
+++ models/Like.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-//check
-console.log("hi")
+
 
 const LikeSchema = new mongoose.Schema({
     recipe: { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe', required: true },
